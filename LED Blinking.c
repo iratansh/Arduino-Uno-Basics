6ae195af90ef75cab8804ad137cdc3404786d3f8
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-12-25 18:33:45 -0700

    Update LED Blinking.c

--- LED Blinking.c
+++ LED Blinking.c
@@ -1,4 +1,4 @@
-#define LED_PIN 13
+#define LED_PIN 3
 void setup() {
     pinMode(LED_PIN, OUTPUT);
 }
@@ -10,6 +10,13 @@ void loop() {
 }
 
 // Notes:
+// For Making the LED remain on:
 // Connect the resistor to the long end of the LED (anode)
 // Connect the short leg of the LED to GND
 // The other side of the resistor should be connected to the 5V pin
+
+
+// For Making the LED flicker:
+// Connect the resistor to the long end of the LED (anode)
+// Connect the short leg of the LED to GND on the top of the uno
+// The other side of the resistor should be connected to Pin 3
